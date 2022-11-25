21da2d2583bec489085a54567eaa7901e78517ee
NAAADIR <nadir.boumazaa@gmail.com>
2022-11-25 11:22:15 +0100

    ..
    .

---
+++ app/Http/Controllers/Api/V1/HotelController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Hotel;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Hotel::orderByDesc('created_at')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (Hotel::create($request->all())) {
+            return response()->json([
+                'success' => 'Crée avec succès'
+            ], 200);
+        }
+        else{
+            return response()->json([
+                'error'
+            ], 200);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -40,3 +40,22 @@ Route::apiResource('payment', App\Http\Controllers\PaymentController::class);
 Route::apiResource('payment-type', App\Http\Controllers\PaymentTypeController::class);
 
 
+
+
+Route::apiResource('bedroom', App\Http\Controllers\BedroomController::class);
+
+Route::apiResource('bedroom-type', App\Http\Controllers\BedroomTypeController::class);
+
+Route::apiResource('benefit', App\Http\Controllers\BenefitController::class);
+
+Route::apiResource('benefit-price', App\Http\Controllers\BenefitPriceController::class);
+
+Route::apiResource('booking', App\Http\Controllers\BookingController::class);
+
+Route::apiResource('hotel', App\Http\Controllers\HotelController::class);
+
+Route::apiResource('hotel-class', App\Http\Controllers\HotelClassController::class);
+
+Route::apiResource('payment', App\Http\Controllers\PaymentController::class);
+
+Route::apiResource('payment-type', App\Http\Controllers\PaymentTypeController::class);
